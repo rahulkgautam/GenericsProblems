dd03e9d26a8d324e14c2196103dafb93339fa9d5
rahulkgautam <rahulkgautam125@gmail.com>
2023-07-26 22:58:25 +0530

    Find max value using compareTo method in Int

---
+++ Max.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GenericsProblem
+{
+    class Max
+    {
+        public static void FindMax(int first, int second, int third)
+        {
+            if (first.CompareTo(second) >= 0 && first.CompareTo(third) >= 0)
+                Console.WriteLine("First is max {0}",first);
+            else if (second.CompareTo(first) >= 0 && second.CompareTo(third) >= 0)
+                Console.WriteLine("Second is max {0}", second);
+            else
+                Console.WriteLine("Third is max {0}", third);
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -6,7 +6,22 @@ namespace GenericsProblem
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Welcome to Generics Problems Program");
+            //TC_!
+            //int firstNumber = 20;
+            //int secondNumber = 19;
+            //int thirdNumber = 12;
+
+            //TC_2
+            //int firstNumber = 20;
+            //int secondNumber = 39;
+            //int thirdNumber = 10;
+
+            //TC_3
+            int firstNumber = 20;
+            int secondNumber = 19;
+            int thirdNumber = 40;
+            Max.FindMax(firstNumber,secondNumber,thirdNumber);
+            
         }
     }
 }
